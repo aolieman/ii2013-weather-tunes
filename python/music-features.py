3b4e339735f5c02ea5c0a2bec01ae59d3efd527a
Alex Olieman <alex@olieman.net>
2013-02-24 18:36:27 +0100

    Metadata retrieval for last.fm tracks now also works for problematic unicode titles. Sample input and output for two geocharts in /json

--- python/music-features.py
+++ python/music-features.py
@@ -3,25 +3,24 @@ import io, json, urllib2
 LASTFM_KEY = '126da10c7b76db388c8537edbac24c6c'
 failedlist = []
 
-def track_info(mbid, arso, outfile):
-    if mbid != None:
-        query = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key='\
-            + LASTFM_KEY + '&mbid=' + mbid + '&format=json'
-    elif arso != None:
-        artist = urllib2.quote(arso[0].encode('ascii', 'replace'))
-        song = urllib2.quote(arso[1].encode('ascii', 'replace'))
-        query = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key='\
-            + LASTFM_KEY + '&artist=' + artist + '&track=' + song + '&format=json'
-    else: print '!! Provide either an mbid or an arso to query'    
+def track_info(tracktuple, outfile):
+    if tracktuple[0] != '':
+        mbid = '&mbid=' + tracktuple[0]
+    else: mbid = ''
+    artist = urllib2.quote(tracktuple[1].encode('utf-8'))
+    songtitle = urllib2.quote(tracktuple[2].encode('utf-8'))
+    query = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key='\
+        + LASTFM_KEY + mbid + '&artist=' + artist + '&track='\
+        + songtitle + '&format=json'
     response = json.loads(urllib2.urlopen(query).read())
     result = None
     try:
         result = response['track']
     except KeyError:
         global failedlist
-        print '?? No result for', mbid
-        print response
-        failedlist.append(mbid)
+        print '?? No result for', tracktuple
+        print '   ', response
+        failedlist.append(tracktuple)
     if result != None:
         trinfo = {}
         trinfo['track'] = {}
@@ -44,30 +43,25 @@ def track_info(mbid, arso, outfile):
         print trinfo['track']['name'], 'succesfully appended'
 
 def get_tracks(chart):
-    mbidlist = []
-    arsolist = []
+    tracklist = []
     jchart = json.load(open(chart))
     for track in jchart['toptracks']['track']:
-        if len(track['mbid']) > 1:
-            mbidlist.append(track['mbid'])
-        else:
-            arsolist.append((track['artist']['name'], track['name']))
-    return mbidlist, arsolist
+        tracklist.append((track['mbid'], track['artist']['name'], track['name']))
+    return tracklist
     
 
 if __name__ == '__main__' :
 
-    chart = '../json/geochart.json'
+    chart = '../json/geochart_ams.json'
 
     ## execute and save
-    outfile = open('../json/trackinfo.json', 'wb')
-    trlist, arsolist = get_tracks(chart)
-    print '++ Adding tracks by mbid'
-    for mbid in trlist:
-        track_info(mbid, None, outfile)
-    print '++ Adding tracks by arso'
-    for arso in arsolist:
-        track_info(None, arso, outfile)
+    outfile = open('../json/trackinfo_ams.json', 'wb')
+    trlist = get_tracks(chart)
+    print '++ Adding tracks by from tracklist'
+    for tracktuple in trlist:
+        track_info(tracktuple, outfile)
     outfile.close()
 
-    print failedlist
+    if len(failedlist) == 0:
+        print '!! Everything succeeded'
+    else: print failedlist
